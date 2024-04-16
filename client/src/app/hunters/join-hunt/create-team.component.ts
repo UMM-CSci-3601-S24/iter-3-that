7dd3828c3d385a56637988308689120b86311921
mahon479 <mahon479@morris.umn.edu>
2024-04-16 14:01:35 -0500

    Added createTeam() to the create-team.component on client side

---
+++ client/src/app/hunters/join-hunt/create-team.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { Router, RouterLink } from "@angular/router";
+import { HostService } from "src/app/hosts/host.service";
+
+@Component({
+  selector: 'app-create-team',
+  templateUrl: './create-team.component.html',
+  styleUrls: ['./create-team.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink]
+})
+export class CreateTeamComponent {
+  teamForm: FormGroup;
+
+  constructor(private hostService: HostService, private fb: FormBuilder, private router: Router) {
+    this.teamForm = this.fb.group({
+      teamName: ['', Validators.required],
+      memberNames: ['', Validators.required]
+    });
+  }
+
+  createTeam(): void {
+    if (this.teamForm.valid) {
+      const teamName = this.teamForm.get('teamName').value;
+      const memberNames = this.teamForm.get('memberNames').value.split(',').map(name => name.trim());
+
+      this.hostService.createTeam(teamName, memberNames).subscribe(response => {
+        this.router.navigate(['/team', response.teamId]);
+      }, error => {
+        console.error('Error creating team:', error);
+      });
+    }
+  }
+}
