b826a9c4a527f5a23960e54f356a2cae0ca201f0
Ely Johnson <elyjohnson6@gmail.com>
2024-04-20 17:07:12 -0500

    Reformatted hunt card ts and html

--- client/src/app/hunts/hunt-card.component.ts
+++ client/src/app/hunts/hunt-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input} from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,31 +7,49 @@ import { Router, RouterLink } from '@angular/router';
 import { Hunt } from './hunt';
 import { CommonModule } from '@angular/common';
 import { HostService } from '../hosts/host.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-    selector: 'app-hunt-card',
-    templateUrl: './hunt-card.component.html',
-    styleUrls: ['./hunt-card.component.scss'],
-    standalone: true,
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink, MatFormField, MatLabel, MatError, ReactiveFormsModule]
+  selector: 'app-hunt-card',
+  templateUrl: './hunt-card.component.html',
+  styleUrls: ['./hunt-card.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    RouterLink,
+    MatFormField,
+    MatInputModule,
+    MatLabel,
+    MatError,
+    ReactiveFormsModule,
+  ],
 })
 export class HuntCardComponent {
-
-
   hunt = input.required<Hunt>();
   simple = input(true);
   math = Math;
 
-
   NumberofTeamsForm = new FormGroup({
-    numTeam: new FormControl<number>(null, Validators.compose([
-      Validators.required,
-      Validators.min(1),
-      Validators.max(20),
-      Validators.pattern('^[0-9]+$')
-    ])),
+    numTeam: new FormControl<number>(
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.min(1),
+        Validators.max(20),
+        Validators.pattern('^[0-9]+$'),
+      ])
+    ),
   });
 
   readonly numberOfTeamsValidationMessages = {
@@ -39,8 +57,8 @@ export class HuntCardComponent {
       { type: 'required', message: 'Please provide the number of teams' },
       { type: 'min', message: 'Number of teams must be at least 1' },
       { type: 'max', message: 'Cannot have more than 20 teams' },
-      { type: 'pattern', message: 'Must be a number' }
-    ]
+      { type: 'pattern', message: 'Must be a number' },
+    ],
   };
 
   @Input() context: 'host-profile' | 'hunt-profile' = 'hunt-profile';
@@ -59,12 +77,19 @@ export class HuntCardComponent {
   }
 
   formControlHasError(controlName: string): boolean {
-    return this.NumberofTeamsForm.get(controlName).invalid &&
-      (this.NumberofTeamsForm.get(controlName).dirty || this.NumberofTeamsForm.get(controlName).touched);
+    return (
+      this.NumberofTeamsForm.get(controlName).invalid &&
+      (this.NumberofTeamsForm.get(controlName).dirty ||
+        this.NumberofTeamsForm.get(controlName).touched)
+    );
   }
 
-  getErrorMessage(numTeam: keyof typeof this.numberOfTeamsValidationMessages): string {
-    for(const {type, message} of this.numberOfTeamsValidationMessages[numTeam]) {
+  getErrorMessage(
+    numTeam: keyof typeof this.numberOfTeamsValidationMessages
+  ): string {
+    for (const { type, message } of this.numberOfTeamsValidationMessages[
+      numTeam
+    ]) {
       if (this.NumberofTeamsForm.get(numTeam).hasError(type)) {
         return message;
       }
@@ -72,5 +97,3 @@ export class HuntCardComponent {
     return 'Unknown error';
   }
 }
-
-
