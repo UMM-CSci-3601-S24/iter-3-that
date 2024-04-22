f84634b6b42a51d2a969346e57c6df40283c5a00
alijawosti <alijawosti@gmail.com>
2024-04-22 17:28:02 -0500

    Change model for hunt

--- client/src/app/hunts/completeHunt.ts
+++ client/src/app/hunts/completeHunt.ts
@@ -1,7 +1,10 @@
 import { Hunt } from "./hunt";
 import { Task } from "./task";
+import { Team } from "./team";
+
 
 export interface CompleteHunt {
   hunt: Hunt;
   tasks: Task[];
+  teams: Team[];
 }
---
+++ client/src/app/hunts/team.ts
@@ -0,0 +1,6 @@
+export interface Team {
+  _id: string;
+  huntId: string;
+  name: string;
+  progress: number;
+}
