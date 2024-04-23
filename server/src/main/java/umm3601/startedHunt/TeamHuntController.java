e0ca3cc37b395e17a40a691ffe1d3b6d5fc1badc
LinneaMay <Linneamay@gilbertson.net>
2024-04-23 14:19:12 -0500

    #26 added websocket method and route for updating teamHunts

--- server/src/main/java/umm3601/startedHunt/TeamHuntController.java
+++ server/src/main/java/umm3601/startedHunt/TeamHuntController.java
@@ -6,6 +6,12 @@ import org.bson.UuidRepresentation;
 import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 import com.mongodb.client.MongoDatabase;
 
@@ -14,17 +20,23 @@ import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import io.javalin.http.NotFoundResponse;
+import io.javalin.websocket.WsContext;
 import umm3601.Controller;
+
 
 
 //none of this will work until teamhunts is merged with main or merged into the branch
 
 public class TeamHuntController implements Controller {
   private static final String API_TEAMHUNTS_BY_INVITE_CODE = "api/teamhunts/{invitecode}";
-  //private static final String TEAMHUNT_UPDATES_WEBSOCKET = "web"
+  private static final String TEAMHUNT_UPDATES_WEBSOCKET = "ws/teamhunts{startedhuntid}";
 
   static final String INVITE_CODE_KEY = "accessCode";
   static final String STARTED_HUNT_ID_KEY = "startedHuntId";
+
+  //the string value that is associated with the WsContexts is the started HuntId that all team hunts can be gotten from
+  //put there for ease of access in functions
+  private HashMap<WsContext, String> connectedContextsDictionary = new HashMap<WsContext, String>();
 
   private final JacksonMongoCollection<StartedHunt> startedHuntCollection;
   private final JacksonMongoCollection<TeamHunt> TeamHuntCollection;
@@ -69,9 +81,35 @@ private TeamHunt[] getTeamHuntsByStartedHuntId(String startedHuntId) {
   return(teamHuntsArray);
 }
 
+
+private void updateTeamHuntsViaWebsocket(String startedHuntId) {
+  Iterator<WsContext> iterator = connectedContextsDictionary.keySet().iterator(); //  connectedContexts.iterator();
+  while (iterator.hasNext()) {
+    WsContext ws = iterator.next();
+    if (ws.session.isOpen()) {
+      String associatedStartedHuntID = connectedContextsDictionary.get(ws);
+      if(associatedStartedHuntID.equals(startedHuntId)) {
+        TeamHunt[] teamHuntsArray = getTeamHuntsByStartedHuntId(startedHuntId);
+        ws.send(teamHuntsArray);
+      } else {
+        iterator.remove();
+      }
+    }
+  }
+}
+
 public void addRoutes(Javalin server) {
   //get all teamhunts for a started hunt
   server.get(API_TEAMHUNTS_BY_INVITE_CODE, this::getTeamHuntsByInviteCode);
+
+  //get live updates of teamhunts and task progress
+  server.ws(TEAMHUNT_UPDATES_WEBSOCKET, ws -> {
+    ws.onConnect(ctx -> {
+      String startedHuntId = ctx.pathParam("startedhuntid");
+      connectedContextsDictionary.put(ctx, startedHuntId);
+      ctx.enableAutomaticPings(5, TimeUnit.SECONDS);
+    });
+  });
 }
 
 
