c0d947365b00fac14af1d50c1cda5770b37aefe9
Ely Johnson <elyjohnson6@gmail.com>
2024-04-22 21:37:34 -0500

    Tests for create-team.component

--- client/src/app/hunters/join-hunt/create-team.component.spec.ts
+++ client/src/app/hunters/join-hunt/create-team.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { of } from 'rxjs';
@@ -8,11 +8,12 @@ import { HostService } from 'src/app/hosts/host.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CreateTeamComponent', () => {
-  let component: CreateTeamComponent;
+  let createTeamComponent: CreateTeamComponent;
   let fixture: ComponentFixture<CreateTeamComponent>;
   let mockHostService;
   let mockRouter;
   let mockSnackBar;
+  let teamForm: FormGroup;
 
   beforeEach(async () => {
     mockHostService = jasmine.createSpyObj(['createTeam']);
@@ -37,30 +38,92 @@ describe('CreateTeamComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateTeamComponent);
-    component = fixture.componentInstance;
+    createTeamComponent = fixture.componentInstance;
     fixture.detectChanges();
+    teamForm = createTeamComponent.teamForm;
+    expect(teamForm).toBeDefined();
+    expect(teamForm.controls).toBeDefined();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the component and form', () => {
+    expect(createTeamComponent).toBeTruthy();
+    expect(teamForm).toBeTruthy();
+  });
+
+  it('form should be invalid when empty', () => {
+    expect(teamForm.valid).toBeFalsy();
+  });
+
+  describe('The teamName field', () => {
+    let nameControl: AbstractControl;
+
+    beforeEach(() => {
+      nameControl = createTeamComponent.teamForm.controls.teamName;
+    });
+
+    it('should not allow empty names', () => {
+      nameControl.setValue('');
+      expect(nameControl.valid).toBeFalsy();
+    });
+
+    it('should be fine with "The Best Hunt"', () => {
+      nameControl.setValue('The Best Hunt');
+      expect(nameControl.valid).toBeTruthy();
+    });
+
+    it('should fail on really long names', () => {
+      nameControl.setValue('t'.repeat(100));
+      expect(nameControl.valid).toBeFalsy();
+      expect(nameControl.hasError('maxlength')).toBeTruthy();
+    });
+
+    it('should allow digits in the name', () => {
+      nameControl.setValue('Bad2Th3B0ne');
+      expect(nameControl.valid).toBeTruthy();
+    });
+  });
+
+  describe('The members field', () => {
+    let membersArray: FormArray;
+
+    beforeEach(() => {
+      membersArray = createTeamComponent.teamForm.get('members') as FormArray;
+    });
+
+    it('should not allow empty names', () => {
+      const memberControl = membersArray.at(0);
+      memberControl.setValue('');
+      expect(memberControl.valid).toBeFalsy();
+    });
+
+    it('should be fine with "The Best Hunt"', () => {
+      const memberControl = membersArray.at(0);
+      memberControl.setValue('The Best Hunt');
+      expect(memberControl.valid).toBeTruthy();
+    });
+
+    it('should fail on really long names', () => {
+      const memberControl = membersArray.at(0);
+      memberControl.setValue('t'.repeat(100));
+      expect(memberControl.valid).toBeFalsy();
+      expect(memberControl.hasError('maxlength')).toBeTruthy();
+    });
+
+    it('should allow digits in the name', () => {
+      const memberControl = membersArray.at(0);
+      memberControl.setValue('Bad2Th3B0ne');
+      expect(memberControl.valid).toBeTruthy();
+    });
   });
 
   it('should create a team when the form is valid', () => {
     mockHostService.createTeam.and.returnValue(of('123'));
-    component.teamForm.setValue({ teamName: 'Test Team', members: ['Test Member'], startedHuntId: '' });
+    createTeamComponent.teamForm.setValue({ teamName: 'Test Team', members: ['Test Member'], startedHuntId: '' });
 
-    component.submitForm();
+    createTeamComponent.submitForm();
 
-    expect(mockHostService.createTeam).toHaveBeenCalledWith(component.teamForm.value);
+    expect(mockHostService.createTeam).toHaveBeenCalledWith(createTeamComponent.teamForm.value);
     expect(mockSnackBar.open).toHaveBeenCalledWith('Team created successfully', 'Close', { duration: 5000 });
     expect(mockRouter.navigate).toHaveBeenCalledWith(['/team', '123']);
-  });
-
-  it('should not create a team when the form is invalid', () => {
-    component.teamForm.setValue({ teamName: '', members: [''], startedHuntId: '' });
-
-    component.submitForm();
-
-    expect(mockHostService.createTeam).not.toHaveBeenCalled();
   });
 });
