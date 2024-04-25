374a0beb408b8ae72686c2600ff8a5dd9aaf116e
mahon479 <mahon479@morris.umn.edu>
2024-04-25 13:16:06 -0500

    Added tests for edit-hunt-component

--- client/src/app/hunts/edit-hunt.component.spec.ts
+++ client/src/app/hunts/edit-hunt.component.spec.ts
@@ -1,42 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditHuntComponent } from './edit-hunt.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { HostService } from '../hosts/host.service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
+//import { HostService } from '../hosts/host.service';
+//import { MatSnackBar } from '@angular/material/snack-bar';
+//import { Router } from '@angular/router';
 
 describe('EditHuntComponent', () => {
   let component: EditHuntComponent;
-  let fixture: ComponentFixture<EditHuntComponent>;
   let mockHostService;
   let mockSnackBar;
   let mockRouter;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     mockHostService = jasmine.createSpyObj(['updateHunt']);
     mockSnackBar = jasmine.createSpyObj(['open']);
     mockRouter = jasmine.createSpyObj(['navigate', 'navigateByUrl']);
 
-    await TestBed.configureTestingModule({
-      declarations: [ EditHuntComponent ],
-      imports: [ ReactiveFormsModule ],
-      providers: [
-        { provide: HostService, useValue: mockHostService },
-        { provide: MatSnackBar, useValue: mockSnackBar },
-        { provide: Router, useValue: mockRouter },
-        { provide: MAT_DIALOG_DATA, useValue: { huntToEdit: { name: 'Test Hunt', description: 'Test Description', est: 120 } } },
-        { provide: MatDialogRef, useValue: {} }
-      ]
-    })
-    .compileComponents();
-  });
+    mockRouter.navigateByUrl.and.returnValue(Promise.resolve(true));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditHuntComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new EditHuntComponent(
+      { huntToEdit: {
+        name: 'Test Hunt', description: 'Test Description', est: 120,
+        _id: '',
+        hostId: '',
+        numberOfTasks: 0
+      } },
+      mockHostService,
+      mockSnackBar,
+      mockRouter
+    );
   });
 
   it('should create', () => {
@@ -44,10 +35,12 @@ describe('EditHuntComponent', () => {
   });
 
   it('should initialize form with hunt data', () => {
+    component.ngOnInit();
     expect(component.editHuntForm.value).toEqual({ name: 'Test Hunt', description: 'Test Description', est: 120 });
   });
 
   it('should submit form', () => {
+    component.ngOnInit();
     mockHostService.updateHunt.and.returnValue(of({}));
     component.submitForm();
     expect(mockHostService.updateHunt).toHaveBeenCalled();
@@ -59,4 +52,46 @@ describe('EditHuntComponent', () => {
     component.navigateToHosts();
     expect(mockRouter.navigate).toHaveBeenCalledWith(['/hosts']);
   });
+
+  it('should return true if form control has error and is dirty or touched', () => {
+    component.ngOnInit();
+    const control = component.editHuntForm.get('name');
+    control.setValue('');
+    control.markAsDirty();
+    expect(component.formControlHasError('name')).toBeTrue();
+    control.markAsPristine();
+    control.markAsTouched();
+    expect(component.formControlHasError('name')).toBeTrue();
+  });
+
+  it('should return false if form control has no error', () => {
+    component.ngOnInit();
+    component.editHuntForm.get('name').setValue('Valid Name');
+    expect(component.formControlHasError('name')).toBeFalse();
+  });
+
+  it('should return error message if form control has error', () => {
+    component.ngOnInit();
+    component.editHuntForm.get('name').setValue('');
+    expect(component.getErrorMessage('name')).toBe('Name is required');
+  });
+
+  it('should return "Unknown error" if form control has no error', () => {
+    component.ngOnInit();
+    component.editHuntForm.get('name').setValue('Valid Name');
+    expect(component.getErrorMessage('name')).toBe('Unknown error');
+  });
+
+  it('should handle error when submitting form', () => {
+    component.ngOnInit();
+    const errorResponse = { status: 500, message: 'Server error' };
+    mockHostService.updateHunt.and.returnValue(throwError(errorResponse));
+    component.submitForm();
+    expect(mockSnackBar.open).toHaveBeenCalledWith(
+      `Problem contacting the server – Error Code: ${errorResponse.status}\nMessage: ${errorResponse.message}`,
+      'OK',
+      { duration: 5000 }
+    );
+  });
+
 });
