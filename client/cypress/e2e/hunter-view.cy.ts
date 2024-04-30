240e0b43e752e9ecdf0431f149a949f06629518e
Andrew Lam <lam00139@morris.umn.edu>
2024-04-30 13:39:39 -0500

    fix failing e2e and take out not needed button

--- client/cypress/e2e/hunter-view.cy.ts
+++ client/cypress/e2e/hunter-view.cy.ts
@@ -343,42 +343,7 @@ describe('Hunter View', () => {
 
     page.getTakePictureButton().first().click();
     page.clickCaptureImageButton();
-    // page.getSnackBar().contains('Photo replaced successfully');
-  });
-
-  it('should cancel capture image when click the overlay', () => {
-    page.getHostButton().click();
-    page.getHuntCards().first().then(() => {
-      page.clickViewProfile(page.getHuntCards().first());
-      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
-    });
-
-    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
-    page.clickBeginHunt();
-    cy.wait(2000);
-    page.getAccessCode();
-
-    // Those above will navigate to the Hunt, begin it
-    // and capture the access code.
-
-    cy.get('@accessCode').then((accessCode) => {
-      cy.visit(`/hunters/`);
-      for (let i = 0; i < accessCode.length; i++) {
-        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
-      }
-    }).then(() => {
-      cy.wait(1000);
-      page.clickJoinHuntButton();
-    });
-
-    page.getTeamNameField().type('Team 1');
-    page.getTeamMembersField().type('John', {force: true});
-    page.clickCreateTeamButton();
-
-    // navigate to the hunter view page with access code.
-
-    page.getTakePictureButton().first().click();
-    page.getCancelOverlay().click( { force: true });
+    page.getSnackBar().contains('Photo uploaded successfully');
   });
 
 });
