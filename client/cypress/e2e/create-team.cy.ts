b5e92dfabd806a5e30afe42d63583248ee5d3898
Andrew Lam <lam00139@morris.umn.edu>
2024-04-30 14:18:30 -0500

    adding some tests case and the support ts for create team page.

---
+++ client/cypress/e2e/create-team.cy.ts
@@ -0,0 +1,166 @@
+import { CreateTeamPage } from "cypress/support/create-team.po";
+
+const page = new CreateTeamPage();
+
+describe('Hunter View', () => {
+  beforeEach(() => page.navigateTo());
+
+  /**
+   * These test will get to the hunt, begin it and capture the access code.
+   * Then it will navigate to the create team page with the access code.
+   */
+
+  it('should navigate to the create team page with the captured access code', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getTeamNameField().type('Team 1');
+    page.getTeamMembersField().type('John', {force: true});
+    page.clickCreateTeamButton();
+
+    page.getHunterViewTitle().contains('You are in');
+  });
+
+  it('should display create team page title', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getCreateTeamTitle().contains('Create Team');
+  });
+
+  it('should display the team name field', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getTeamNameField().should('exist');
+  });
+
+  it('should display the team members field', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getTeamMembersField().should('exist');
+  });
+
+  it('should display the create team button', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getCreateTeamButton().should('exist');
+  });
+
+  it('should display the adding team member button', () => {
+    page.getHostButton().click();
+    page.getHuntCards().first().then(() => {
+      page.clickViewProfile(page.getHuntCards().first());
+      cy.url().should('match', /\/hunts\/[0-9a-fA-F]{24}$/);
+    });
+
+    cy.get('mat-form-field [formcontrolname=numTeam]').type('2', {force: true});
+    page.clickBeginHunt();
+    cy.wait(2000);
+    page.getAccessCode();
+
+    cy.get('@accessCode').then((accessCode) => {
+      cy.visit(`/hunters/`);
+      for (let i = 0; i < accessCode.length; i++) {
+        page.getAccessCodeInput(i + 1).type(accessCode.toString().charAt(i));
+      }
+    }).then(() => {
+      cy.wait(1000);
+      page.clickJoinHuntButton();
+    });
+
+    page.getAddingMembersButton().should('exist');
+  });
+});
---
+++ client/cypress/support/create-team.po.ts
@@ -0,0 +1,270 @@
+export class CreateTeamPage {
+  private readonly baseUrl = '/';
+  private readonly huntTitle = '.hunt-title';
+  private readonly huntNofTasks = '.hunt-Nof-tasks';
+  private readonly huntTimer = '.hunt-timer';
+  private readonly huntTaskList = '.task-list';
+  private readonly HunterButton = '[name="hunter-button"]';
+  private readonly HostButton = '[name="host-button"]';
+  private readonly hostCardSelector = '.hunt-cards-container app-hunt-card';
+  private readonly profileButtonSelector = '[data-test=viewProfileButton]';
+  private readonly beginHuntButton = '.begin-hunt';
+  private readonly huntAccessCode = '.access-code-number';
+  private readonly takePictureButton = '.take-picture';
+  private readonly joinHuntButtonSelector = '[name="join-button"]';
+  private readonly captureImageButton = '.capture-image-button';
+  private readonly cancelCaptureImageButton = '.cancel-capture-button';
+  private readonly snackBar = '.mat-mdc-snack-bar-label.mdc-snackbar__label';
+  private readonly cancelOverlay = '.overlay';
+  private readonly teamNameField = '.test-team-name-input';
+  private readonly teamMembersField = '.team-member-input';
+  private readonly createTeamButton = '.submit-button';
+  private readonly cancelCreateTeamButton = '.cancel-button';
+  private readonly createTeamTitle = '.add-team-title';
+  private readonly addingMembersButton = '.add-team-title';
+
+  navigateTo() {
+    return cy.visit(this.baseUrl);
+  }
+
+  /**
+   * Get the title of the hunter view page.
+   *
+   * @return the value of the element with the ID `.hunt-title`
+   */
+  getHunterViewTitle() {
+    return cy.get(this.huntTitle);
+  }
+
+  /**
+   * Get the hunter button DOM element.
+   *
+   * @return the value of the element with the ID `[name="hunter-button"]`
+   */
+  getHunterButton() {
+    return cy.get(this.HunterButton);
+  }
+
+  /**
+   * Get the host button DOM element.
+   *
+   * @return the value of the element with the ID `[name="host-button"]`
+   */
+  getHostButton() {
+    return cy.get(this.HostButton);
+  }
+
+  /**
+   * Get all the `app-host-card` DOM elements. This will be
+   * empty if we're using the list view of the hosts.
+   *
+   * @returns an iterable (`Cypress.Chainable`) containing all
+   *   the `app-host-card` DOM elements.
+   */
+  getHuntCards() {
+    return cy.get(this.hostCardSelector);
+  }
+
+  /**
+   * Clicks the "view profile" button for the given host card.
+   * Requires being in the "card" view.
+   *
+   * @param card The host card
+   */
+  clickViewProfile(card: Cypress.Chainable<JQuery<HTMLElement>>) {
+    return card.find<HTMLButtonElement>(this.profileButtonSelector).click();
+  }
+
+  /**
+   * Clicks the "begin hunt" button in the hunt page.
+   * Requires being in the "hunt" view as hosts.
+   *
+   * @returns the value of the element with the ID `.begin-hunt`
+   */
+  clickBeginHunt() {
+    return cy.get(this.beginHuntButton).click();
+  }
+
+  /**
+   * Get the access code from the started Hunt.
+   * Requires begin Hunt in the "hunt" view as hosts.
+   *
+   * @returns the value of the element with the class `.col-md-12 Access Code`
+   */
+  getAccessCode() {
+    return cy.get(this.huntAccessCode).invoke('text').as('accessCode');
+  }
+
+  /**
+   * Get the number of tasks of the hunt as hunter-view.
+   *
+   * @returns the value of the element with the class `.hunt`
+   */
+  getHuntNofTasks() {
+    return cy.get(this.huntNofTasks);
+  }
+
+  /**
+   * Get the number of tasks of the hunt as hunter-view.
+   *
+   * @returns the value of the element with the class `.hunt`
+   */
+  getHuntTimer() {
+    return cy.get(this.huntTimer);
+  }
+
+  /**
+   * Get the task list of the hunt as hunter-view.
+   *
+   * @return the value of the element with the class `.task-list`
+   */
+  getHuntTaskList() {
+    return cy.get(this.huntTaskList);
+  }
+
+  /**
+   * Get the take picture button.
+   *
+   * @returns the value of the element.
+   */
+  getTakePictureButton() {
+    return cy.get(this.takePictureButton);
+  }
+
+  /**
+   * Get the position of the box in the access input field.
+   *
+   * @param index
+   * @returns
+   */
+  getAccessCodeInput(index: number) {
+    return cy.get(`.input-container input:nth-child(${index})`);
+  }
+
+  /**
+   * Click the join-hunt button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  clickJoinHuntButton() {
+    return cy.get(this.joinHuntButtonSelector).click();
+  }
+
+  /**
+   * Get the capture image button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getCaptureImageButton() {
+    return cy.get(this.captureImageButton);
+  }
+
+  /**
+   * Click the capture image button DOM element.
+   *
+   * @return the value of the element with the class.
+   */
+  clickCaptureImageButton() {
+    return cy.get(this.captureImageButton).click();
+  }
+
+  /**
+   * Get the cancel capture image button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getCancelCaptureButton() {
+    return cy.get(this.cancelCaptureImageButton);
+  }
+
+  /**
+   * Click the cancel capture image button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  clickCancelCaptureButton() {
+    return cy.get(this.cancelCaptureImageButton).click();
+  }
+
+  /**
+   * Get the snack bar DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getSnackBar() {
+    cy.wait(500);
+    return cy.get(this.snackBar);
+  }
+
+  /**
+   * Get the cancel overlay DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getCancelOverlay() {
+    return cy.get(this.cancelOverlay);
+  }
+
+  /**
+   * Get the team field DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getTeamNameField() {
+    return cy.get(this.teamNameField);
+  }
+
+  /**
+   * Get the team members field DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getTeamMembersField() {
+    return cy.get(this.teamMembersField);
+  }
+
+  /**
+   * Click the create team button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  clickCreateTeamButton() {
+    return cy.get(this.createTeamButton).click();
+  }
+
+  /**
+   * Click the cancel create team button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  clickCancelCreateTeamButton() {
+    return cy.get(this.cancelCreateTeamButton).click();
+  }
+
+  /**
+   * Get the create team title DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getCreateTeamTitle() {
+    return cy.get(this.createTeamTitle);
+  }
+
+  /**
+   * Get the create team button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getCreateTeamButton() {
+    return cy.get(this.createTeamButton);
+  }
+
+  /**
+   * Get the add member button DOM element.
+   *
+   * @returns the value of the element with the class.
+   */
+  getAddingMembersButton() {
+    return cy.get(this.addingMembersButton);
+  }
+}
